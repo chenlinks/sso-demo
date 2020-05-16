14b569b7ac1bee37cf40cf8e8f2bb1748c6938ca
chenling <chenlingys@live.com>
2020-05-16 16:12:24 +0800

    code init

--- src/main/java/com/epxing/demo/controller/SsoController.java
+++ src/main/java/com/epxing/demo/controller/SsoController.java
@@ -1,7 +1,6 @@
 package com.epxing.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.epxing.demo.config.AppRuntimeEnv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +11,13 @@ import java.util.Map;
 
 /**
  * @author chenling
- * @date 2020/5/12 21:49
+ * @date 2020/5/16 16:10
  * @since V1.0.0
  */
+
 @RestController
 @RequestMapping("/api/v1/test")
 public class SsoController {
-
-    @Autowired
-    private AppRuntimeEnv appRuntimeEnv;
 
     @GetMapping("/sso")
     public String testSso(){
@@ -28,8 +25,6 @@ public class SsoController {
         Map<String,Object> result = new HashMap<>(4);
         result.put("code","00001");
         result.put("msg","ok");
-        result.put("userName",appRuntimeEnv.getUsername());
-        result.put("token",appRuntimeEnv.getToken());
         return JSONObject.toJSONString(result);
 
     }
